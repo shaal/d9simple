38a61329a75d7ce678f4656d22363b1591cd26c3
Randy Fay <randy@randyfay.com>
2021-11-27 09:17:34 -0700

    Update to current

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1087,8 +1087,8 @@
         'psr/container-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0',
-                1 => '^1.0',
+                0 => '^1.0',
+                1 => '1.0',
             ),
         ),
         'psr/event-dispatcher-implementation' => array(
